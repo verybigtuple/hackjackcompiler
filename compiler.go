b5d5bad49e0f065232b5f5bd021e906861b24fa3
verybigtuple <bigtuple@gmail.com>
2021-06-06 19:03:43 +0300

    Added functions in compiler to generate push/pop/function/call/do/return

--- compiler.go
+++ compiler.go
@@ -1,10 +1,72 @@
 package main
 
+import (
+	"strconv"
+	"strings"
+)
+
+type MemSegment string
+
+const (
+	ConstSegm MemSegment = "constant"
+	TempSegm             = "temp"
+)
+
+var ops = map[string]string{
+	"+": "add",
+	"-": "sub",
+	"&": "and",
+	"|": "or",
+	"=": "eq",
+	">": "gt",
+	"<": "lt",
+}
+
+// Operations that should use OS functions
+var sysOps = map[string]string{
+	"*": "Math.multiply",
+	"/": "Math.divide",
+}
+
 type Compiler struct {
+	sb            *strings.Builder
 	SymbolTblList *SymbolTableList
 }
 
 func NewCompiler() *Compiler {
 	stList := NewSymbolTableList()
-	return &Compiler{stList}
+	sb := &strings.Builder{}
+	return &Compiler{sb, stList}
+}
+
+func (c *Compiler) String() string {
+	return c.sb.String()
+}
+
+func (c *Compiler) Push(segm MemSegment, offset string) {
+	c.sb.WriteString("push " + string(segm) + " " + offset + "\n")
+}
+
+func (c *Compiler) Pop(segm MemSegment, offset string) {
+	c.sb.WriteString("pop " + string(segm) + " " + offset + "\n")
+}
+
+func (c *Compiler) Function(name string, localVarCount int) {
+	c.sb.WriteString("function " + name + " " + strconv.Itoa(localVarCount) + "\n")
+}
+
+func (c *Compiler) Call(name string, argsCount int) {
+	c.sb.WriteString("call " + name + " " + strconv.Itoa(argsCount) + "\n")
+}
+
+func (c *Compiler) Return() {
+	c.sb.WriteString("return \n")
+}
+
+func (c *Compiler) Op(symbol string) {
+	if cmd, ok := ops[symbol]; ok {
+		c.sb.WriteString(cmd + "\n")
+	} else if sf, ok := sysOps[symbol]; ok {
+		c.Call(sf, 2)
+	}
 }
