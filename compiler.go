6822a13ee6c1ef3eaa7c2b9160725af53e6574d8
verybigtuple <bigtuple@gmail.com>
2021-06-12 19:27:06 +0300

    Simlifyed if and while labels

--- compiler.go
+++ compiler.go
@@ -144,17 +144,15 @@ func (c *Compiler) IfGoto(label string) {
 
 // OpenWhile returns 2 label names for beginWhile and endWhile
 func (c *Compiler) OpenWhile() (begin, end string) {
-	fn := c.Tbl.Name()
-	begin = fn + "$WHILE_BEGIN_" + strconv.Itoa(c.whileCount)
-	end = fn + "$WHILE_END_" + strconv.Itoa(c.whileCount)
+	begin = "WHILE_BEGIN_" + strconv.Itoa(c.whileCount)
+	end = "WHILE_END_" + strconv.Itoa(c.whileCount)
 	c.whileCount++
 	return
 }
 
 func (c *Compiler) OpenIf() (els, end string) {
-	fn := c.Tbl.Name()
-	els = fn + "$ELSE_" + strconv.Itoa(c.ifCount)
-	end = fn + "$IF_END_" + strconv.Itoa(c.ifCount)
+	els = "ELSE_" + strconv.Itoa(c.ifCount)
+	end = "IF_END_" + strconv.Itoa(c.ifCount)
 	c.ifCount++
 	return
 }
